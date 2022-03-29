9ddbc0fd265933e6ea6170d24c8c32ae70e57a5a
voldev8 <voldev@hotmail.com>
2022-03-29 13:46:12 -0400

    register and login user, jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,8 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/userModel');
 
 // @desc   Register a new user
 // @route  /api/users
@@ -10,15 +14,66 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please include all fields');
   }
-  res.send('Register route');
+
+  // Find if user exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists.');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc   Login a new user
 // @route  /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login route');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select('+password');
+
+  // Check if user and password match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid Credentials');
+  }
 });
+
+// Generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
 
 module.exports = {
   registerUser,
