e8017ddd78866571d682d570617c6807bca8857c
voldev8 <voldev@hotmail.com>
2022-03-29 11:44:07 -0400

    basic express server setup

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { loginUser, registerUser } = require('../controllers/userController');
+
+router.post('/', registerUser);
+router.post('/login', loginUser);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT;
+
+const app = express();
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+// Routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
