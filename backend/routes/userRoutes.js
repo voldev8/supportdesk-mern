887cde986d476928bcf82c98cab3372abfd09fc3
voldev8 <voldev@hotmail.com>
2022-03-29 14:10:32 -0400

    protect routes & auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { loginUser, registerUser } = require('../controllers/userController');
+const {
+  loginUser,
+  registerUser,
+  getMe,
+} = require('../controllers/userController');
+
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
+router.get('/me', protect, getMe);
 
 module.exports = router;
